6e16d17268343e5a4c09a2f2cf315235a6647e8a
root <root@linux.78ad68.com>
2025-03-21 16:39:16 +0000

    加入幻觉检测和相关度检测双重验证

---
+++ rag_relevant_hallucination.py
@@ -0,0 +1,280 @@
+from fastapi import FastAPI
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langserve import add_routes
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage, trim_messages
+from langchain_core.chat_history import BaseChatMessageHistory, InMemoryChatMessageHistory
+from langchain_core.prompts import (
+    ChatPromptTemplate,
+    FewShotChatMessagePromptTemplate,
+)
+from operator import itemgetter
+import os  
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import START, MessagesState, StateGraph
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from typing import Sequence
+
+from langchain_core.messages import BaseMessage
+from langgraph.graph.message import add_messages
+from typing_extensions import Annotated, TypedDict
+from typing import Sequence
+from langchain_core.messages import BaseMessage
+from langgraph.graph.message import add_messages
+from typing_extensions import Annotated, TypedDict
+from typing import Sequence
+from langchain_core.messages import BaseMessage
+from langgraph.graph.message import add_messages
+from typing_extensions import Annotated, TypedDict
+from langchain.chat_models import init_chat_model
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.vectorstores import InMemoryVectorStore
+import bs4
+from langchain import hub
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langgraph.graph import START, StateGraph
+from typing_extensions import List, TypedDict
+import getpass
+from typing import Literal
+from typing_extensions import Annotated
+from langchain_core.prompts import PromptTemplate
+from langchain_core.tools import tool
+from langgraph.graph import MessagesState, StateGraph
+from langchain_core.messages import SystemMessage
+from langgraph.prebuilt import ToolNode
+from langgraph.graph import END
+from langgraph.prebuilt import ToolNode, tools_condition
+from pydantic import BaseModel , Field
+
+
+
+os.environ["TOGETHER_API_KEY"] = "sk-jpbyUPV7oHGME9IiE4cSbP4hoKsTHESPoZQM7R3FRgxwBfOl"
+
+llm = ChatOpenAI(
+    base_url="https://api2.aigcbest.top/v1",
+    api_key=os.environ["TOGETHER_API_KEY"],
+    model="gpt-3.5-turbo",
+)
+
+embeddings = OpenAIEmbeddings(
+    base_url="https://api2.aigcbest.top/v1",
+    api_key=os.environ["TOGETHER_API_KEY"],
+    model="text-embedding-3-large"
+)
+
+
+class GradeDocuments(BaseModel):
+    """Binary score for relevance check on retrieved documents."""
+
+    binary_score: str = Field(
+        description="Documents are relevant to the question, 'yes' or 'no'"
+    )
+        
+# LLM with function call
+llm_grader = ChatOpenAI(
+    base_url="https://api2.aigcbest.top/v1",
+    api_key=os.environ["TOGETHER_API_KEY"],
+    model="gpt-4o-mini",
+)
+
+structured_llm_grader = llm_grader.with_structured_output(GradeDocuments)
+
+system = """You are a grader assessing relevance of a retrieved document to a user question. \n 
+    If the document contains keyword(s) or semantic meaning related to the user question, grade it as relevant. \n
+    It does not need to be a stringent test. The goal is to filter out erroneous retrievals. \n
+    Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question."""
+
+grade_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system),
+        ("human", "Retrieved document: \n\n {document} \n\n User question: {question}"),
+    ]
+)
+
+retrieval_grader = grade_prompt | structured_llm_grader
+
+
+
+# 设置 LangSmith 追踪环境变量
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
+os.environ["LANGCHAIN_API_KEY"] = "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"
+os.environ["LANGCHAIN_PROJECT"] = "pr-wooden-organisation-94"
+
+
+urls =[
+    "https://www.deeplearning.ai/the-batch/how-agents-can-improve-llm-performance/?ref=dl-staging-website.ghost.io",
+    "https://www.deeplearning.ai/the-batch/agentic-design-patterns-part-3-tool-use/?ref=dl-staging-website.ghost.io",
+    "https://www.deeplearning.ai/the-batch/agentic-design-patterns-part-2-reflection/?ref=dl-staging-website.ghost.io",
+    "https://www.deeplearning.ai/the-batch/agentic-design-patterns-part-4-planning/?ref=dl-staging-website.ghost.io",
+    "https://www.deeplearning.ai/the-batch/agentic-design-patterns-part-5-multi-agent-collaboration/?ref=dl-staging-website.ghost.io"
+]
+
+docs = [WebBaseLoader(url).load() for url in urls]
+
+docs_list = [item for sublist in docs for item in sublist]
+
+
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=500, chunk_overlap=0
+)
+
+doc_splits = text_splitter.split_documents(docs_list)
+
+vector_store = InMemoryVectorStore(embeddings)
+_ = vector_store.add_documents(doc_splits)
+
+
+graph_builder = StateGraph(MessagesState)
+
+
+@tool(response_format="content_and_artifact")
+def retrieve(question: str):
+    """Retrieve information related to a query."""
+    retrieved_docs = vector_store.similarity_search(question, k=4)
+    
+    docs_to_use = []
+
+    for doc in retrieved_docs:
+        res = retrieval_grader.invoke(
+            {"question": question, "document": doc.page_content}
+        )
+        print(res)
+        if res.binary_score == "yes":
+            docs_to_use.append(doc)
+        
+        
+    serialized = "\n\n".join(
+        (f"Source: {doc.metadata}\n" f"Content: {doc.page_content}")
+        for doc in docs_to_use
+    )
+    
+    return serialized, retrieved_docs
+
+
+
+def query_or_respond(state: MessagesState):
+    """Generate tool call for retrieval or respond."""
+    llm_with_tools = llm.bind_tools([retrieve])
+    response = llm_with_tools.invoke(state["messages"])
+    
+    # MessagesState appends messages to state instead of overwriting
+    return {"messages": [response]}
+
+
+# Step 2: Execute the retrieval.
+tools = ToolNode([retrieve])
+
+
+# Step 3: Generate a response using the retrieved content.
+def generate(state: MessagesState):
+    """Generate answer."""
+    # Get generated ToolMessages
+    recent_tool_messages = []
+    for message in reversed(state["messages"]):
+        if message.type == "tool":
+            recent_tool_messages.append(message)
+        else:
+            break
+    tool_messages = recent_tool_messages[::-1]
+
+    # Format into prompt
+    docs_content = "\n\n".join(doc.content for doc in tool_messages)
+    system_message_content = (
+        "You are an assistant for question-answering tasks. "
+        "Use the following pieces of retrieved context to answer "
+        "the question. If you don't know the answer, say that you "
+        "don't know. Use three sentences maximum and keep the "
+        "answer concise."
+        "\n\n"
+        f"{docs_content}"
+    )
+    conversation_messages = [
+        message
+        for message in state["messages"]
+        if message.type in ("human", "system")
+        or (message.type == "ai" and not message.tool_calls)
+    ]
+    prompt = [SystemMessage(system_message_content)] + conversation_messages
+
+    # Run
+    response = llm.invoke(prompt)
+    return {"messages": [response]}
+
+
+graph_builder.add_node(query_or_respond)
+graph_builder.add_node(tools)
+graph_builder.add_node(generate)
+
+graph_builder.set_entry_point("query_or_respond")
+graph_builder.add_conditional_edges(
+    "query_or_respond",
+    tools_condition,
+    {END: END, "tools": "tools"},
+)
+graph_builder.add_edge("tools", "generate")
+graph_builder.add_edge("generate", END)
+
+graph = graph_builder.compile()
+
+
+template = "{question}"
+custom_rag_prompt = PromptTemplate.from_template(template)
+
+
+def prompt_to_state(prompt_output):
+    question_text = str(prompt_output)
+    return {
+        "messages": [HumanMessage(content=question_text)]
+    }
+
+
+
+# def prompt_to_ans(generations):
+#     for message in generations["messages"]:
+#         if isinstance(message, AIMessage):
+#             return message.content
+#     return None  
+
+        
+       
+
+#  从 State 中提取 answer
+def prompt_to_ans(state):
+    for message in state["generate"]["messages"]:
+        if isinstance(message, AIMessage):
+            return message.content
+    return None  
+
+parser = StrOutputParser()
+
+chain = custom_rag_prompt  | prompt_to_state | graph |prompt_to_state 
+# | prompt_to_ans
+
+#   | parser
+
+app = FastAPI(
+  title="LangChain Server",
+  version="1.0",
+  description="A simple API server using LangChain's Runnable interfaces",
+)
+
+
+add_routes(
+    app,
+    chain,
+    path="/chain",
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=9000)
+
+
+
+
